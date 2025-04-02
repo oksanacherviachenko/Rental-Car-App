e6213146ea7e78a6a926bb7a492f051ae520a249
Oksana Cherviachenko <kvintinovna@gmail.com>
2025-04-03 01:52:33 +0200

    update app.jsx and pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
 import Loader from './components/Loader/Loader';
 
+// Сторінки RentalCar
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
-const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
-const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const CarDetailsPage = lazy(() => import('./pages/CarDetailsPage/CarDetailsPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
+
+// 🔄 Компоненти з Movie-проєкту, залишаємо закоментованими на майбутнє (можеш видалити пізніше)
+// const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+// const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+// const MovieCast = lazy(() => import('./components/MovieCast/MovieCast'));
+// const MovieReviews = lazy(() => import('./components/MovieReviews/MovieReviews'));
 
 function App() {
   return (
@@ -18,12 +23,16 @@ function App() {
         <Suspense fallback={<Loader />}>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/movies" element={<MoviesPage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:id" element={<CarDetailsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+
+            {/* 🔄 Закоментовано на випадок майбутнього розширення */}
+            {/* <Route path="/movies" element={<MoviesPage />} />
             <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
               <Route path="cast" element={<MovieCast />} />
               <Route path="reviews" element={<MovieReviews />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
+            </Route> */}
           </Routes>
         </Suspense>
       </div>
